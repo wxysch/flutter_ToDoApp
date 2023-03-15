f09b585a36abfbc1fb64d13afc409293c964a538
alinu1 <uwu1343@gmail.com>
2023-03-16 00:11:29 +0600

    Create Add ToDo Item Section Using Stack Widget

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -11,62 +11,108 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: tdBGColor,
-      appBar: _buildAppBar(),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 20,vertical: 15),
-        child: Column(children: [
-          searchBox(),
-          Expanded(
-            child: ListView(
-            children: [
-              Container(
-                margin: EdgeInsets.only(
-                  top: 50,
-                  bottom: 20,
+        backgroundColor: tdBGColor,
+        appBar: _buildAppBar(),
+        body: Stack(
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              child: Column(children: [
+                searchBox(),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(
+                          top: 50,
+                          bottom: 20,
+                        ),
+                        child: Text(
+                          'All ToDos',
+                          style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                      for (ToDo todoo in todosList)
+                        ToDoItem(
+                          todo: todoo,
+                        ),
+                    ],
+                  ),
+                )
+              ]),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Row(children: [
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.only(
+                      bottom: 20, right: 20, left: 20
+                      ),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 20, vertical: 5
+                      ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.grey,
+                          offset: Offset(0.0, 0.0),
+                          blurRadius: 10.0,
+                          spreadRadius: 0.0,
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintText: 'Add a new todo item',
+                          border: InputBorder.none),
+                    ),
+                  ),
                 ),
-                child: Text(
-                  'All ToDos',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.w500
-                ),
-                ),
-              ),
-
-              for (ToDo todoo in todosList)
-                ToDoItem(todo: todoo,),
-            ],
-          ), 
+                Container(
+                  margin: EdgeInsets.only(
+                    bottom: 20,
+                    right: 20,
+                  ),
+                  child: ElevatedButton(
+                    child: Text('+',style: TextStyle(fontSize: 40,),),
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: tdBlue,
+                      minimumSize: Size(60, 60),
+                      elevation: 10,
+                    ), 
+                    ),
+                )
+              ]),
             )
-        ]),
-      ),
-    );
+          ],
+        ));
   }
 
   Widget searchBox() {
     return Container(
-            padding: EdgeInsets.symmetric(horizontal: 15),
-            decoration: BoxDecoration(
-                color: Colors.white, borderRadius: BorderRadius.circular(20)),
-            child: TextField(
-              decoration: InputDecoration(
-                contentPadding: EdgeInsets.all(0),
-                prefixIcon: Icon(
-                  Icons.search,
-                  color: tdBlack,
-                  size: 20,
-                ),
-                prefixIconConstraints: 
-                BoxConstraints(
-                  maxHeight: 20, 
-                minWidth: 25),
-                border: InputBorder.none,
-                hintText: 'Search',
-                hintStyle: TextStyle(color: tdGrey),
-              ),
-            ),
-          );
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(20)),
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(0),
+          prefixIcon: Icon(
+            Icons.search,
+            color: tdBlack,
+            size: 20,
+          ),
+          prefixIconConstraints: BoxConstraints(maxHeight: 20, minWidth: 25),
+          border: InputBorder.none,
+          hintText: 'Search',
+          hintStyle: TextStyle(color: tdGrey),
+        ),
+      ),
+    );
   }
 
   AppBar _buildAppBar() {
